3b7e0f063bc2253b379532475da4097c21a7a015
Soumya Dash <M1053566@mindtree.com>
2020-11-04 21:06:57 +0530

    .

--- src/app.js
+++ src/app.js
@@ -4,22 +4,21 @@ const path = require('path');
 const express = require('express');
 const app = new express();
 
+const { accounts, users, writeJSON } = require('./data.js');
+
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
+
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
-
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
-const users = JSON.parse(userData);
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
